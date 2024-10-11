65b0399b29fb4bd47958d435d71ea17db359fd3a
Sheik Raseeth Ansar <129539396+Raseeth123@users.noreply.github.com>
2024-10-11 09:24:37 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,9 +9,9 @@ export default function Navbar() {
         </Link>
         <Link
           className="bg-white text-indigo-600 font-semibold py-2 px-6 rounded-md shadow-lg hover:bg-indigo-100 transition-all duration-300 ease-in-out"
-          href={"/addTopic"}
+          href={"/addProduct"}
         >
-          Add Topic
+          Add Product
         </Link>
       </div>
     </nav>
